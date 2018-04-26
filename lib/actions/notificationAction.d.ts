374ba120170bf1c45ffe9f3cbdf3eac9a1b39290
dmitry.yudin95@gmail.com <dmitry.yudin95@gmail.com>
2018-04-26 16:39:04 +0700

    fix type

--- lib/actions/notificationAction.d.ts
+++ lib/actions/notificationAction.d.ts
@@ -2,7 +2,7 @@ import { Action } from "redux";
 
 export const SHOW_NOTIFICATION = 'RA/SHOW_NOTIFICATION';
 
-export function showNotification(message: string, type: string, notificationOptions: any): Action
+export function showNotification(message: string, type?: string, notificationOptions?: any): Action
 export const HIDE_NOTIFICATION = 'RA/HIDE_NOTIFICATION';
 
 export function hideNotification(): Action;
